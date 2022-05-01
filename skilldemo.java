85e10ccb7908b9b5c468db1f77455bd87999f0f2
StrawberryAkai <zihun1392@gmail.com>
2022-05-01 16:55:02 -0700

    skilldemo commit

--- skilldemo.java
+++ skilldemo.java
@@ -1,4 +1,7 @@
 public class skilldemo {
 
+    public static int sub(int i, int j){
+        return i - j;
+    }
 
 }
---
+++ skilldemotest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class skilldemotest {
+    @Test
+    public void sub(){
+        assertEquals(2, skilldemo.sub(5, 2));
+    }
+}
